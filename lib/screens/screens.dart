95590695bc55e72b62ec8b214cd099dd2fdeedc7
CITLALI_ABIGAIL_OCHOA_GOMEZ <a.20308051281074@cbtis128.edu.mx>
2023-05-16 22:20:08 +0000

    .

---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+// Nos servira para importar las pantallas del proyecto
+
+export "package:flutternavegacion/screens/primary_screen.dart";
+export "package:flutternavegacion/screens/secondary_screen.dart";
